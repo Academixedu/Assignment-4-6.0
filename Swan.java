fef37ff205d854cbf5b2014421711818e88b7344
jahnavi0545 <kandiboyinajahnavi@gmail.com>
2024-07-18 18:53:01 +0530

    interface

--- Swan.java
+++ Swan.java
@@ -1,3 +1,19 @@
 public class Swan{
 // Implement desired interfaces and write necessary implementations
+public class Swan implements Walkable,Flyable,Swimmable{
+    public void swim(){
+        System.out.println("swan can Swim");
+    }
 }
+public void fly(){
+    System.out.println("swan can fly");
+}
+public void walk(){
+    System.out.println("swan can walk");
+}
+public void name(){
+    System.out.println("swan name is :"+name);
+}
+}    
+
+
