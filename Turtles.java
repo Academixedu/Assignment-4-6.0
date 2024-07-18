869093edc550a8b19fe22f86af7aa628ed95ea63
siva-245711 <sivanagendra809@gmail.com>
2024-07-18 19:24:51 +0530

    java

--- Turtles.java
+++ Turtles.java
@@ -1,3 +1,44 @@
-public class Turtles{
+public class Turtles implements Flyable,Swimmable,Walkable{
 // implement desired interface and make implementations to it
+
+    @Override
+    public void fly() {
+        System.out.println("turtles cannot fly");
+   }   
+
+    @Override
+    public void swim() {
+        System.out.println("turtles can swim");
+   }
+
+    @Override
+    public void walk() {
+        System.out.println("turtles cannot walk");
+
+   }    
+   @Override
+   public void name(String name) {
+    System.out.println("name of the anaimal is "+name);
+       
+   }   
+   
+
+    @Override
+    public void name( ) {
+        
+    }
+
+    @Override
+    public void speed() {
+        System.out.println("speed of the anaimal is 5km/h");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("turtles eat vegatables");
+    }
+
+    
+    
+
 }
