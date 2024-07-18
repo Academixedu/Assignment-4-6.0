620b229467216906660b822c5ff46fbf2465fe16
Naveentogaru1 <togarunaveen1@gmail.com>
2024-07-18 18:28:11 +0530

    Interface.assin/Main.java

--- Turtles.java
+++ Turtles.java
@@ -1,3 +1,12 @@
 public class Turtles{
 // implement desired interface and make implementations to it
+public void swim(){
+    System.out.println("swimming");
+}
+
+}
+class Horse extends Turtles{
+    public void walk(){
+        System.out.println("walking");
+    }
 }
