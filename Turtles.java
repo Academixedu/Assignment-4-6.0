90d4f946db6a2039ced67059f3aff3aebfd4db5b
gsbhavani <suryagogadi02@gmail.com>
2024-07-18 12:17:03 +0530

    interface

--- Turtles.java
+++ Turtles.java
@@ -1,3 +1,19 @@
-public class Turtles{
+public class Turtles implements Swimmable, Walkable, Flyable{
 // implement desired interface and make implementations to it
+public void walk()
+{
+  System.out.println("Turtle can Walk");
+}
+public void swim()
+{
+  System.out.println("Turtle can Swim");
+}
+public void fly()
+{
+    System.out.println("Turtle can't Fly");
+}
+public void name(String name)
+{
+    System.out.println("Turtle name is "+name);
+}
 }
