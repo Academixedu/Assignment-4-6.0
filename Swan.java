82f9712c68cc54ea78f5095208718c22bd200401
sudeep992718 <sdutpala@gmail.com>
2024-07-18 14:44:55 +0530

    Sudeep Interface

--- Swan.java
+++ Swan.java
@@ -1,3 +1,27 @@
-public class Swan{
+public class Swan implements Swimmable, Flyable, Walkable{
+
+    @Override
+    public void walk() {
+        // TODO Auto-generated method stub
+        System.out.println("Swam cannot Walk");
+    }
+
+    @Override
+    public void fly() {
+        // TODO Auto-generated method stub
+        System.out.println("Swans can Fly");
+    }
+
+    @Override
+    public void swim() {
+        // TODO Auto-generated method stub
+        System.out.println("Swans can Swim");
+    }
+
+    @Override
+    public void name(String name) {
+        // TODO Auto-generated method stub
+        System.out.println("Swan species is realated Avian family of animal kingdom. " +"The relative name is "+name);
+    }
 // Implement desired interfaces and write necessary implementations
 }
--- Walkable.java
+++ Walkable.java
@@ -1,3 +1,5 @@
 public interface Walkable{
-// Write Functions in this interface
-}
+    // Write Functions in this interface
+    void walk();
+    }
+    
