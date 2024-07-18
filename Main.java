3d24a0d362c0bba8b592fa8aa0804e0e97f5b658
srinadh0405 <srinadh.nallabothula@email.com>
2024-07-18 12:36:06 +0530

    Assignment Done

--- Main.java
+++ Main.java
@@ -1,4 +1,17 @@
 public class Main{
   public static void main(String []args){
     // All Objects Creations and Method Callings Should do here
-  }}
+    Swan swan = new Swan();
+    swan.fly();
+    swan.swim();
+    swan.walk();
+    swan.name("Swan");
+
+    Turtles turtles= new Turtles();
+    turtles.swim();
+    turtles.walk();
+    turtles.name("Turtles");
+
+
+  }
+}
