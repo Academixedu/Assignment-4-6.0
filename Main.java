98a784cb727268619c8d9f441bf7bc1f50fe0083
rishiyendhar <rishyendhar.taduri@gmail.com>
2024-07-18 17:39:29 +0530

    PolyTest_Rishi

--- Main.java
+++ Main.java
@@ -1,4 +1,18 @@
 public class Main{
   public static void main(String []args){
-    // All Objects Creations and Method Callings Should do here
-  }}
+    Turtles t1 = new Turtles();
+    t1.fly();
+    t1.swim();
+    t1.walk();
+    t1.name("Pinchu");
+
+    Swan s1 = new Swan();
+    s1.fly();
+    s1.swim();
+    s1.walk();
+    s1.name("Punchu");
+
+    
+  }
+
+}
--- Swan.java
+++ Swan.java
@@ -1,3 +1,23 @@
-public class Swan{
-// Implement desired interfaces and write necessary implementations
+public class Swan implements Flyable, Walkable, Swimmable{
+
+    @Override
+    public void swim() {
+        System.out.println("A Swan can swim");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("A Swan might or might not walk");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("A Swan can Fly");
+    }
+
+    @Override
+    public void name(String name) {
+        System.out.println("This animal's name is Swan"  + name);
+    }
+
 }
